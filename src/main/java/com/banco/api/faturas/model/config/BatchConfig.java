ad8f1cad5660ac7e212277270dd2c475707305f9
samuelmoreira05 <samuel442005@gmail.com>
2025-12-31 15:18:46 -0300

    feat: finalizando os Steps de fatura

--- src/main/java/com/banco/api/faturas/model/config/BatchConfig.java
+++ src/main/java/com/banco/api/faturas/model/config/BatchConfig.java
@@ -2,6 +2,12 @@ package com.banco.api.faturas.model.config;
 
 import com.banco.api.faturas.model.Fatura;
 import com.banco.api.faturas.model.dto.FaturaDTO;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.data.MongoItemWriter;
 import org.springframework.batch.item.data.builder.MongoItemWriterBuilder;
@@ -11,6 +17,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.jdbc.core.DataClassRowMapper;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 import java.time.LocalDateTime;
@@ -48,6 +55,29 @@ public class BatchConfig {
         return new MongoItemWriterBuilder<Fatura>()
                 .template(mongoTemplate)
                 .collection("faturas")
+                .build();
+    }
+
+    @Bean
+    public Step faturaStep(JobRepository jobRepository,
+                           PlatformTransactionManager transactionManager,
+                           JdbcCursorItemReader<FaturaDTO> reader,
+                           ItemProcessor<FaturaDTO, Fatura> processor,
+                           MongoItemWriter<Fatura> writer){
+
+        return new StepBuilder("faturaStep", jobRepository)
+                .<FaturaDTO, Fatura>chunk(10, transactionManager)
+                .reader(reader)
+                .processor(processor)
+                .writer(writer)
+                .build();
+    }
+
+    @Bean
+    public Job faturaJob(JobRepository jobRepository, Step faturaStep){
+        return new JobBuilder("geracaoFaturaJob", jobRepository)
+                .start(faturaStep)
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
 }
